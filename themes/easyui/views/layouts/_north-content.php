03df341fa32e4f389d8829521f66c28e28796405
sheillendra <sheillendra@yahoo.com>
2015-11-08 08:18:02 +0700

    init

--- themes/easyui/views/layouts/_north-content.php
+++ themes/easyui/views/layouts/_north-content.php
@@ -1,5 +1,5 @@
 <div id="nort-content" style="padding-top:5px;padding-right:5px;">
-    <div style="float:left;padding-left:5px;color:#2d6987;"><strong style="font-size: 18px;">My Company</strong></div>
+    <div style="float:left;padding-left:5px;color:#2d6987;"><strong style="font-size: 18px;"><?=Yii::$app->name?></strong></div>
     <div id="north-right-nav">
         <a id="north-user-menu" data-options="iconCls:\'icon-user\'">
             <?php echo Yii::$app->user->identity->username; ?>
