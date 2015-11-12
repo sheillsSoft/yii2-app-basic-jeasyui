1d3786b231c71e9c2993db536b1920448b4977cb
sheillendra <sheillendra@yahoo.com>
2015-11-12 15:26:53 +0700

    init

--- environments/dev/config/db-local.php
+++ environments/dev/config/db-local.php
@@ -1,11 +1,9 @@
 <?php
 
 return [
-    /*
     'class' => 'yii\db\Connection',
-    'dsn' => 'pgsql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
     'username' => 'root',
     'password' => '1234',
     'charset' => 'utf8',
-     */
 ];
---
+++ environments/dev/config/web-local.php
@@ -0,0 +1,26 @@
+<?php
+
+$params = require(__DIR__ . '/params-local.php');
+
+$config = [
+
+    'components' => [
+        'db' => require(__DIR__ . '/db-local.php'),
+    ],
+    'params' => $params,
+];
+
+if (YII_ENV_DEV) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [
+        'class' => 'yii\debug\Module',
+    ];
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+    ];
+}
+
+return $config;
